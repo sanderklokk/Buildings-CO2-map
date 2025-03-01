f14f8d96fa90e6bc65dac8a617d7c46db77eb043
mork-e <159708381+mork-e@users.noreply.github.com>
2025-03-01 17:49:10 +0100

    feat: Legge login til router

--- Frontend/src/pages/Router.tsx
+++ Frontend/src/pages/Router.tsx
@@ -1,23 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./Home";
+import { Login } from "./Login";
 
 export const Router = () => {
+  const routes = [
+    {
+      name: "Home",
+      path: "/",
+      component: <Home />,
+    },
+    {
+      name: "Login",
+      path: "/login",
+      component: <Login />,
+    },
+  ];
 
-    const routes = [
-        {
-            name: 'Home',
-            path: '/',
-            component: <Home/>
-        }
-    ]
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                {routes.map((route) => (
-                    <Route key={route.name} path={route.path} element={route.component} />
-                ))}
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.name} path={route.path} element={route.component} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
+};
