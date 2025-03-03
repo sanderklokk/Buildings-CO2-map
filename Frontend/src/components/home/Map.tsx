103e101c4cfa0f93e5f686f52f73aa934b2f740e
akselsf <akselsf>
2025-03-03 10:50:20 +0100

    style: Oppdatert kartstil

--- Frontend/src/components/home/Map.tsx
+++ Frontend/src/components/home/Map.tsx
@@ -16,9 +16,10 @@ export const Map = () => {
       zoom={13}
       scrollWheelZoom={true}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      <TileLayer url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        subdomains='abcd'
+        maxZoom={20}
       />
       {data &&
         data.map((pos, i) => (
