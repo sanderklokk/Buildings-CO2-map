9153b89731ab7f5b81365503b61836643c6aa04e
mork-e <159708381+mork-e@users.noreply.github.com>
2025-03-07 19:19:25 +0100

    refactor: overgang til MUI

--- Frontend/src/components/home/Map.tsx
+++ Frontend/src/components/home/Map.tsx
@@ -1,31 +1,26 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import { HeatLayer } from "./MapLayers/HeatLayer";
 import { MarkerLayer } from "./MapLayers/MarkerLayer";
-
-// temporary
 import { getData } from "../../../../Data/getdata.ts";
 
 export const Map = () => {
-  
- const data = getData();
- // const data = [{ lat: 63.43049, long: 10.39506, text: "Trondheim", intensity: 40 }];
+  const data = getData();
 
   return (
     <MapContainer
-      className="h-full"
+      className="w-full h-full"
       center={[63.43049, 10.39506]}
       zoom={13}
       scrollWheelZoom={true}
     >
       <HeatLayer data={data} />
       <MarkerLayer data={data} />
-      <TileLayer url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
+      <TileLayer
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        subdomains='abcd'
+        subdomains="abcd"
         maxZoom={20}
       />
-    
- 
     </MapContainer>
   );
 };
