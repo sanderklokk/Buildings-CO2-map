c04b73aa7d2af9fb750dc995f64bc98ba0b5f81c
akselsf <akselsf>
2025-03-11 11:15:20 +0100

    refactor: Fix labels over heatmap

--- Frontend/src/components/home/Map.tsx
+++ Frontend/src/components/home/Map.tsx
@@ -1,17 +1,17 @@
-import { MapContainer } from "react-leaflet";
+import { MapContainer, Pane, TileLayer } from "react-leaflet";
 import { HeatLayer } from "./MapLayers/HeatLayer";
 import { MarkerLayer } from "./MapLayers/MarkerLayer";
-import { TileLayer } from "./MapLayers/TileLayer";
+//import { TileLayer } from "./MapLayers/TileLayer";
 
 // temporary
 import { getData } from "../../../../Data/getdata.ts";
 
 
 export const Map = () => {
-  
- const data = getData();
- 
- // const data = [{ lat: 63.43049, long: 10.39506, text: "Trondheim", intensity: 40 }];
+
+  const data = getData();
+
+  // const data = [{ lat: 63.43049, long: 10.39506, text: "Trondheim", intensity: 40 }];
 
   return (
     <MapContainer
@@ -24,29 +24,21 @@ export const Map = () => {
         [62.731479127003986, 12.232074915704278],
       ]}
       minZoom={9}
+      maxZoom={20}
 
-      
     >
-      
-    <TileLayer url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        maxZoom={20}
-        zIndex={1}
-        id={"fullmaplayer"}
-        
-      />
-  
+      <Pane name="fullmappane" style={{ zIndex: 1 }} >
+        <TileLayer url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        />
+      </Pane>
       <HeatLayer data={data} zIndex={2} />
       <MarkerLayer data={data} />
-      
-      <TileLayer url='https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png'
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            maxZoom={20}
-            zIndex={3}
-            id="labelmaplayer"
-          />
-     
-
+      <Pane name="labelpane" style={{ zIndex: 3 }}>
+        <TileLayer url='https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        />
+      </Pane>
     </MapContainer>
   );
 };
