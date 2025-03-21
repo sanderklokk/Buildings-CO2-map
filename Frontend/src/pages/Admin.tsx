9ef6c78eeb894fcf0899aaf8c258f5deb396fc80
mork-e <159708381+mork-e@users.noreply.github.com>
2025-03-21 10:50:30 +0100

    feat: Enkel GUI for admin

--- Frontend/src/pages/Admin.tsx
+++ Frontend/src/pages/Admin.tsx
@@ -1,3 +1,16 @@
+import { Container, Typography, Button, Stack } from "@mui/material";
+
 export const Admin = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Administrasjonspanel
+      </Typography>
+      <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
+        <Button variant="contained">Administrer materialer</Button>
+        <Button variant="outlined">Vis avfallsrapporter</Button>
+      </Stack>
+    </Container>
+  );
 };
+export default Admin;
