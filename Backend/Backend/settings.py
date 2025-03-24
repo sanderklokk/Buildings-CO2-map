ccee2fc984af5871911afe4c827f2be5039113dc
Antony William Mariadason <william.mariadason@gmail.com>
2025-03-24 12:59:43 +0100

    conf: change db engine to postgres in Django settings

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "App"
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,12 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
 
