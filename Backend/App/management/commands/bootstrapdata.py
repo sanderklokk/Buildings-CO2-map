91745794b547ee88e713fccda68c68275d02b4ac
Antony William Mariadason <william.mariadason@gmail.com>
2025-04-08 20:10:55 +0200

    feat: add method to write to bygning table from polars dataframe

--- Backend/App/management/commands/bootstrapdata.py
+++ Backend/App/management/commands/bootstrapdata.py
@@ -19,4 +19,32 @@ class Command(BaseCommand):
 
         return df
 
-   
+    #TO-DO
+    #*Add bulk_create for all tables
+    #*Fix/figiure out duplicate
+    def populate(self, df: polars.DataFrame):
+        bygning.objects.bulk_create([
+            bygning(
+                bygnignsnr=row["properties.bygningsnr"],
+                bygningsstatuskode=row["properties.bygningstatuskode"],
+                bygdDato=row["properties.dato"].split('T', maxsplit = 1)[0],
+                anntalboenheter=row["properties.antallboenheter"],
+                antalletasjer=row["properties.antalletasjer"],
+                bebygdareal=row["properties.bebygdareal"],
+                bruksarealannet=row["properties.bruksarealannet"],
+                bruksarealbolig=row["properties.bruksarealbolig"],
+                bruksarealtotalt=row["properties.bruksarealtotalt"],
+                bygningstypekode=row["properties.bygningstypekode"],
+                kommune=row["properties.kommune"]
+
+            ) for row in df.iter_rows(named=True)
+        ])
+
+    def handle(self, *args, **options):
+        st = time.time()
+        self.stdout.write(os.getcwd())
+        pprint.pp(self.createDf().columns)
+        self.populate(self.createDf())
+        print("print: " + os.getcwd())
+        print("elapsed time: " + str(round(time.time() - st, 3)))
+        
