0fd1b881d180a801199466db58d4ba30a86783e9
Antony William Mariadason <william.mariadason@gmail.com>
2025-04-08 20:10:06 +0200

    feat: add method to read json_files into a joined polars dataframe

--- Backend/App/management/commands/bootstrapdata.py
+++ Backend/App/management/commands/bootstrapdata.py
@@ -7,5 +7,16 @@ import polars
 from App.models import *
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        pass
+    def createDf(self) -> polars.DataFrame:
+        with open('../Data/data14.json', 'r') as file14, open('../Data/data15.json', 'r') as file15:
+            data14 = json.load(file14)
+            data15 = json.load(file15)
+
+        df14 = polars.json_normalize(data14['features']).drop(["type", "geometry.type"])
+        df15 = polars.json_normalize(data15['features']).drop("type")
+
+        df = df14.join(other=df15, on="properties.bygningsnr", how="inner")
+
+        return df
+
+   
