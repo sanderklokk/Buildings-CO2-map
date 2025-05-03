3152062c3940d1907aff4719541f631d02364a21
akselsf <akselsf>
2025-05-03 19:39:02 +0200

    fix: run prettier

--- Frontend/src/components/submitwastereport/AvfallsplanForm.tsx
+++ Frontend/src/components/submitwastereport/AvfallsplanForm.tsx
@@ -24,7 +24,10 @@ export const AvfallsplanForm = () => {
     data: materialtypes,
     isLoading: isMaterialsLoading,
     isError: isMaterialsError,
-  } = useQuery({ queryKey: ["materialtypes", false], queryFn: () => get_all_materialtypes(false) });
+  } = useQuery({
+    queryKey: ["materialtypes", false],
+    queryFn: () => get_all_materialtypes(false),
+  });
 
   const { addAvfallRow, wasteReport } = useBoundStore().wasteReportForm;
 
@@ -42,15 +45,15 @@ export const AvfallsplanForm = () => {
 
     if (addWasteCategoryInputValue === "")
       return materialtypes.data.filter(
-        (wasteMaterial) =>
-          !wasteMaterial.forelder
+        (wasteMaterial) => !wasteMaterial.forelder,
       );
     return materialtypes.data.filter(
       (wasteMaterial) =>
         !wasteReport.avfall.farlig
           .concat(wasteReport.avfall.ordinert)
           .map((x) => x.id)
-          .includes(wasteMaterial.id == null ? -1 : wasteMaterial.id) ||  getSubMaterials(wasteMaterial.id).length > 0
+          .includes(wasteMaterial.id == null ? -1 : wasteMaterial.id) ||
+        getSubMaterials(wasteMaterial.id).length > 0,
     );
   };
 
@@ -59,11 +62,10 @@ export const AvfallsplanForm = () => {
   };
 
   const getSubMaterials = (materialId: number | null) => {
-
     if (!materialtypes) return [];
 
     return materialtypes.data.filter(
-      (wasteMaterial) => wasteMaterial.forelder === materialId
+      (wasteMaterial) => wasteMaterial.forelder === materialId,
     );
   };
 
@@ -75,17 +77,17 @@ export const AvfallsplanForm = () => {
         !wasteReport.avfall.farlig
           .concat(wasteReport.avfall.ordinert)
           .map((x) => x.id)
-          .includes(wasteMaterial.id == null ? -1 : wasteMaterial.id)
+          .includes(wasteMaterial.id == null ? -1 : wasteMaterial.id),
     );
   };
 
-  const handleAddWasteCategory = (value: APIMaterialType | null, force=false) => {
-    
+  const handleAddWasteCategory = (
+    value: APIMaterialType | null,
+    force = false,
+  ) => {
     if (value) {
       if (value.id == null) return;
-     
-     
-     
+
       setSelected(value.id);
       if (getSubMaterials(value.id).length === 0 || force) {
         if (
@@ -98,7 +100,7 @@ export const AvfallsplanForm = () => {
           setAddWasteCategoryInputValue("");
           return;
         }
-        
+
         setShowSubMaterials(false);
         addAvfallRow(value.id, value.farlig);
         setAddWasteCategoryValue(null);
@@ -147,22 +149,24 @@ export const AvfallsplanForm = () => {
                   {materialtypes.data.find((x) => x.id == selected)?.navn}
                 </Typography>
                 <Box>
-                  {(() => { 
+                  {(() => {
                     if (!selected) return;
                     const mat = getMaterialById(selected);
                     if (!mat) return;
-                  
-                      return <Button
-                      key={mat.id}
-                      sx={{ margin: "3px" }}
-                      onClick={() => handleAddWasteCategory(mat, true)}
-                      variant="outlined"
-                      data-testid="reportform-add-parent-material"
-                    >
-                      {mat.navn}
-                    </Button>
+
+                    return (
+                      <Button
+                        key={mat.id}
+                        sx={{ margin: "3px" }}
+                        onClick={() => handleAddWasteCategory(mat, true)}
+                        variant="outlined"
+                        data-testid="reportform-add-parent-material"
+                      >
+                        {mat.navn}
+                      </Button>
+                    );
                   })()}
-                  <Divider orientation="vertical" className="inline"  />
+                  <Divider orientation="vertical" className="inline" />
                   {showSubMaterials &&
                     getSubMaterialsNotInUse().length === 0 && (
                       <Typography>
@@ -177,7 +181,9 @@ export const AvfallsplanForm = () => {
                           sx={{ margin: "3px" }}
                           onClick={() => handleAddWasteCategory(material)}
                           variant="outlined"
-                          data-testid={"reportform-add-sub-material-"+material.id}
+                          data-testid={
+                            "reportform-add-sub-material-" + material.id
+                          }
                         >
                           {material.navn}
                         </Button>
@@ -209,7 +215,10 @@ export const AvfallsplanForm = () => {
         >
           <TableContainer className="max-w-[1200px]">
             <Table>
-              <TableHead className="bg-blue-100 border " data-testid="reportform-ordinert-head">
+              <TableHead
+                className="bg-blue-100 border "
+                data-testid="reportform-ordinert-head"
+              >
                 <TableRow className="">
                   <TableCell>
                     <Typography className="font-bold">
@@ -270,7 +279,7 @@ export const AvfallsplanForm = () => {
                       <AvfallsMaterialeRow
                         key={wasteCategory.id}
                         material={materialtypes?.data.find(
-                          (x) => x.id == wasteCategory.id
+                          (x) => x.id == wasteCategory.id,
                         )}
                       />
                     );
@@ -303,7 +312,10 @@ export const AvfallsplanForm = () => {
         >
           <TableContainer className="max-w-[1200px]">
             <Table>
-              <TableHead className="bg-blue-100 border "  data-testid="reportform-farlig-head">
+              <TableHead
+                className="bg-blue-100 border "
+                data-testid="reportform-farlig-head"
+              >
                 <TableRow className="">
                   <TableCell>
                     <Typography className="font-bold">Farlig avfall</Typography>
@@ -362,7 +374,7 @@ export const AvfallsplanForm = () => {
                       <AvfallsMaterialeRow
                         key={wasteCategory.id}
                         material={materialtypes?.data.find(
-                          (x) => x.id == wasteCategory.id
+                          (x) => x.id == wasteCategory.id,
                         )}
                       />
                     );
--- Frontend/src/tests/components/submitwastereport/GjelderForm.test.tsx
+++ Frontend/src/tests/components/submitwastereport/GjelderForm.test.tsx
@@ -5,81 +5,133 @@ import userEvent from "@testing-library/user-event";
 import { GjelderForm } from "../../../components/submitwastereport/GjelderForm";
 
 describe("GjelderForm", () => {
- 
-    it("check render the form and inputs", () => {
-        render(<GjelderForm />);
-        expect(screen.getByTestId("reportform-gjelder-adresse-inp")).toBeInTheDocument();
-        expect(screen.getByTestId("reportform-gjelder-postnr-inp")).toBeInTheDocument();
-        expect(screen.getByTestId("reportform-gjelder-poststed-inp")).toBeInTheDocument();
-        expect(screen.getByTestId("reportform-gjelder-tiltak-nybygg-radio")).toBeInTheDocument();
-        expect(screen.getByTestId("reportform-gjelder-tiltak-ombygging-radio")).toBeInTheDocument();
-        expect(screen.getByTestId("reportform-gjelder-tiltak-riving-radio")).toBeInTheDocument();
-        expect(screen.getByTestId("reportform-gjelder-berortbra-inp")).toBeInTheDocument();
-        expect(screen.getByTestId("reportform-gjelder-bygningstype-inp")).toBeInTheDocument();
-        expect(screen.getByTestId("reportform-gjelder-konstruksjonstype-inp")).toBeInTheDocument();
-        expect(screen.getByTestId("reportform-gjelder-beskrivelse-inp")).toBeInTheDocument();
+  it("check render the form and inputs", () => {
+    render(<GjelderForm />);
+    expect(
+      screen.getByTestId("reportform-gjelder-adresse-inp"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByTestId("reportform-gjelder-postnr-inp"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByTestId("reportform-gjelder-poststed-inp"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByTestId("reportform-gjelder-tiltak-nybygg-radio"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByTestId("reportform-gjelder-tiltak-ombygging-radio"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByTestId("reportform-gjelder-tiltak-riving-radio"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByTestId("reportform-gjelder-berortbra-inp"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByTestId("reportform-gjelder-bygningstype-inp"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByTestId("reportform-gjelder-konstruksjonstype-inp"),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByTestId("reportform-gjelder-beskrivelse-inp"),
+    ).toBeInTheDocument();
+  });
+
+  it("check correct updating of form state", async () => {
+    render(<GjelderForm />);
+
+    // address,postnr/sted (eiendom/byggested)
+    const address = "kognens gate 3000";
+    const postnr = "5000";
+    const poststed = "trondheim";
+    const inpaddr = within(
+      screen.getByTestId("reportform-gjelder-adresse-inp"),
+    ).getByRole("textbox");
+    await userEvent.type(inpaddr, address);
+    const inppostnr = within(
+      screen.getByTestId("reportform-gjelder-postnr-inp"),
+    ).getByRole("spinbutton");
+    await userEvent.type(inppostnr, postnr);
+    const inppoststed = within(
+      screen.getByTestId("reportform-gjelder-poststed-inp"),
+    ).getByRole("textbox");
+    await userEvent.type(inppoststed, poststed);
+    // tiltaket gjelder (radio,bra,type, beskrivelse)
+    const radionybygg = within(
+      screen.getByTestId("reportform-gjelder-tiltak-nybygg-radio"),
+    ).getByRole("radio");
+    const radioriving = within(
+      screen.getByTestId("reportform-gjelder-tiltak-riving-radio"),
+    ).getByRole("radio");
+    const radiombygging = within(
+      screen.getByTestId("reportform-gjelder-tiltak-ombygging-radio"),
+    ).getByRole("radio");
+
+    await userEvent.click(radionybygg);
+    await waitFor(() => {
+      const { wasteReportForm } = useBoundStore.getState();
+      expect(wasteReportForm.wasteReport.gjelder.tiltak.type).toBe("nybygg");
+    });
+    await userEvent.click(radiombygging);
+    await waitFor(() => {
+      const { wasteReportForm } = useBoundStore.getState();
+      expect(wasteReportForm.wasteReport.gjelder.tiltak.type).toBe("ombygging");
+    });
+    await userEvent.click(radioriving);
+    await waitFor(() => {
+      const { wasteReportForm } = useBoundStore.getState();
+      expect(wasteReportForm.wasteReport.gjelder.tiltak.type).toBe("rivning");
     });
 
-    it("check correct updating of form state", async () => {
-        render(<GjelderForm />);
-    
+    const selectedradio = "rivning";
+    const berortbra = "1000";
+    const bygningstype = "betong";
+    const konstruksjonstype = "betonghus";
+    const beskrivelse =
+      "skal rive hele huset og sende det på trondheim gjenvinningsstasjon";
+    const inpberortbra = within(
+      screen.getByTestId("reportform-gjelder-berortbra-inp"),
+    ).getByRole("textbox");
+    await userEvent.type(inpberortbra, berortbra);
+    const inpbygningstype = within(
+      screen.getByTestId("reportform-gjelder-bygningstype-inp"),
+    ).getByRole("textbox");
+    await userEvent.type(inpbygningstype, bygningstype);
+    const inpkonstruksjonstype = within(
+      screen.getByTestId("reportform-gjelder-konstruksjonstype-inp"),
+    ).getByRole("textbox");
+    await userEvent.type(inpkonstruksjonstype, konstruksjonstype);
+    const inpbeskrivelse = within(
+      screen.getByTestId("reportform-gjelder-beskrivelse-inp"),
+    ).getByRole("textbox");
+    await userEvent.type(inpbeskrivelse, beskrivelse);
 
-        // address,postnr/sted (eiendom/byggested)
-        const address = "kognens gate 3000";
-        const postnr = "5000";
-        const poststed = "trondheim";
-        const inpaddr = within(screen.getByTestId("reportform-gjelder-adresse-inp")).getByRole("textbox");
-        await userEvent.type(inpaddr, address);
-        const inppostnr = within(screen.getByTestId("reportform-gjelder-postnr-inp")).getByRole("spinbutton");
-        await userEvent.type(inppostnr, postnr);
-        const inppoststed = within(screen.getByTestId("reportform-gjelder-poststed-inp")).getByRole("textbox");
-        await userEvent.type(inppoststed, poststed);
-        // tiltaket gjelder (radio,bra,type, beskrivelse)
-        const radionybygg = within(screen.getByTestId("reportform-gjelder-tiltak-nybygg-radio")).getByRole("radio");
-        const radioriving = within(screen.getByTestId("reportform-gjelder-tiltak-riving-radio")).getByRole("radio");
-        const radiombygging = within(screen.getByTestId("reportform-gjelder-tiltak-ombygging-radio")).getByRole("radio");
-        
-        await userEvent.click(radionybygg);
-        await waitFor(() => {
-        const { wasteReportForm } = useBoundStore.getState();
-            expect(wasteReportForm.wasteReport.gjelder.tiltak.type).toBe("nybygg");
-        }
-        );
-        await userEvent.click(radiombygging);   
-        await waitFor(() => {
-            const { wasteReportForm } = useBoundStore.getState();
-            expect(wasteReportForm.wasteReport.gjelder.tiltak.type).toBe("ombygging");
-        });
-        await userEvent.click(radioriving);
-        await waitFor(() => {
-            const { wasteReportForm } = useBoundStore.getState();
-            expect(wasteReportForm.wasteReport.gjelder.tiltak.type).toBe("rivning");
-        });
-
-        const selectedradio = "rivning";
-        const berortbra = "1000"
-        const bygningstype = "betong";
-        const konstruksjonstype = "betonghus";
-        const beskrivelse = "skal rive hele huset og sende det på trondheim gjenvinningsstasjon";
-        const inpberortbra = within(screen.getByTestId("reportform-gjelder-berortbra-inp")).getByRole("textbox");
-        await userEvent.type(inpberortbra, berortbra);
-        const inpbygningstype = within(screen.getByTestId("reportform-gjelder-bygningstype-inp")).getByRole("textbox");
-        await userEvent.type(inpbygningstype, bygningstype);
-        const inpkonstruksjonstype = within(screen.getByTestId("reportform-gjelder-konstruksjonstype-inp")).getByRole("textbox");
-        await userEvent.type(inpkonstruksjonstype, konstruksjonstype);
-        const inpbeskrivelse = within(screen.getByTestId("reportform-gjelder-beskrivelse-inp")).getByRole("textbox");
-        await userEvent.type(inpbeskrivelse, beskrivelse);
-
-        await waitFor(() => {
-            const { wasteReportForm } = useBoundStore.getState();
-            expect(wasteReportForm.wasteReport.gjelder.eiendom.address).toBe(address);
-            expect(wasteReportForm.wasteReport.gjelder.eiendom.postalCode).toBe(postnr);
-            expect(wasteReportForm.wasteReport.gjelder.eiendom.postalPlace).toBe(poststed);
-            expect(wasteReportForm.wasteReport.gjelder.tiltak.type).toBe(selectedradio);
-            expect(wasteReportForm.wasteReport.gjelder.tiltak.berortBRA).toBe(parseFloat(berortbra));
-            expect(wasteReportForm.wasteReport.gjelder.tiltak.bygningstype).toBe(bygningstype);
-            expect(wasteReportForm.wasteReport.gjelder.tiltak.konstruksjonstype).toBe(konstruksjonstype);
-            expect(wasteReportForm.wasteReport.gjelder.tiltak.handtering).toBe(beskrivelse);
-        });
+    await waitFor(() => {
+      const { wasteReportForm } = useBoundStore.getState();
+      expect(wasteReportForm.wasteReport.gjelder.eiendom.address).toBe(address);
+      expect(wasteReportForm.wasteReport.gjelder.eiendom.postalCode).toBe(
+        postnr,
+      );
+      expect(wasteReportForm.wasteReport.gjelder.eiendom.postalPlace).toBe(
+        poststed,
+      );
+      expect(wasteReportForm.wasteReport.gjelder.tiltak.type).toBe(
+        selectedradio,
+      );
+      expect(wasteReportForm.wasteReport.gjelder.tiltak.berortBRA).toBe(
+        parseFloat(berortbra),
+      );
+      expect(wasteReportForm.wasteReport.gjelder.tiltak.bygningstype).toBe(
+        bygningstype,
+      );
+      expect(wasteReportForm.wasteReport.gjelder.tiltak.konstruksjonstype).toBe(
+        konstruksjonstype,
+      );
+      expect(wasteReportForm.wasteReport.gjelder.tiltak.handtering).toBe(
+        beskrivelse,
+      );
     });
+  });
 });
